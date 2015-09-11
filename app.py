b2f2453db69f0d6ce0936659775a6ce0e56b1520
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2015-09-11 12:10:55 -0400

    Remove debug mode

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from wtforms.validators import Required
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask('__name__')
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
