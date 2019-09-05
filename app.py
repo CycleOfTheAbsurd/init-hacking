1e123972696af9b0a83bd7941cc8f95927794eb6
CycleOfTheAbsurd <alex7up42@gmail.com>
2019-09-05 15:52:31 -0400

    Use more recent version of Flask modules.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 
 from flask import Flask, redirect, render_template, request, url_for
-from flask.ext.script import Manager, Shell
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.wtf import Form
+from flask_script import Manager, Shell
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Required
 
